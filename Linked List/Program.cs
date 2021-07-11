66bf3d54b863943f89f29cdc018412827c353652
GollapudiLavanya <lavanyagollapudi5166@gmail.com>
2021-07-11 13:08:20 +0530

    [LAVANYA]Initial Commit

--- Linked List/Program.cs
+++ Linked List/Program.cs
@@ -10,9 +10,9 @@ namespace LinkedListImplementation
             LinkedList linkedList = new LinkedList();
 
             //Adding elements to the list
-            linkedList.Append(30);
             linkedList.Append(56);
             linkedList.Append(70);
+            linkedList.Insert(2, 30);
 
             //Dispaly the list
             linkedList.Display();
