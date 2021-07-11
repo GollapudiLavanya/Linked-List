b24f8828a73377d2e6c3942aa28eda374faa4dfc
GollapudiLavanya <lavanyagollapudi5166@gmail.com>
2021-07-11 13:11:09 +0530

    [LAVANYA]Initial Commit

--- Linked List/LinkedList.cs
+++ Linked List/LinkedList.cs
@@ -82,6 +82,17 @@ namespace LinkedListImplementation
                 temp.Next = newNode;
             }
         }
+
+        //Method to delete first element
+        public void DeleteAtFirst()
+        {
+            if (head == null)
+            {
+                Console.WriteLine("List is empty");
+            }
+            head = head.Next;
+        }
+
         //Method to display elements
         public void Display()
         {
